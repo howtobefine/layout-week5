376744174f0db1342f68e07e4804d03c5981b35e
howtobefine <b4808697b@gmail.com>
2022-08-15 04:27:40 +0800

    index final

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,13 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+$(document).ready(function () {
+  $('.dropdown-all').click(function (e) {
+    event.preventDefault();
+    $('.dropdown-menu-courses').toggleClass('show');
+  })
+})
+
+$(document).ready(function () {
+  $('.reply-btn, .cancel-btn').click(function (e) {
+    event.preventDefault();
+    $('.answer, .assignment-pag, .reply').toggleClass('d-none');
+  })
+})
